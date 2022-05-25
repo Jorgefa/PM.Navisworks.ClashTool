24b72421c9f2b52048bd8f2c23082a52633d8791
PMG-Jorge <jorge.alonso.x@pmgroup-global.com>
2022-05-25 16:22:45 +0200

    Main window added. Initial Naviswork Plugin Template

--- PM.Navisworks.ClashTool/Main.cs
+++ PM.Navisworks.ClashTool/Main.cs
@@ -1,5 +1,8 @@
 ﻿using Autodesk.Navisworks.Api.Plugins;
+using PM.Navisworks.ClashTool.Views;
+using System;
 using System.Windows;
+using System.Windows.Forms.Integration;
 
 namespace PM.Navisworks.ClashTool
 {
@@ -9,12 +12,31 @@ namespace PM.Navisworks.ClashTool
         DisplayName = "Clash\nTool")]
     public class Main : AddInPlugin
     {
+        private static MainWindow _window;
+
         public override int Execute(params string[] parameters)
         {
             var activeDoc = Autodesk.Navisworks.Api.Application.ActiveDocument;
-            MessageBox.Show("Hello!");
+
+            if (_window != null)
+            {
+                _window.Activate();
+            }
+            else
+            {
+                _window = new MainWindow(activeDoc);
+                ElementHost.EnableModelessKeyboardInterop(_window);
+                _window.Closed += WindowClosed;
+                _window.Show();
+            }
+
 
             return 0;
+        }
+
+        private void WindowClosed(object sender, EventArgs e)
+        {
+            _window = null;
         }
     }
 }
---
+++ PM.Navisworks.ClashTool/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Autodesk.Navisworks.Api;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Navisworks.ClashTool.ViewModels
+{
+    public class MainWindowViewModel
+    {
+        public MainWindowViewModel(Document document)
+        {
+            _document = document;
+        }
+
+        private readonly Document _document;
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+       
+
+
+    }
+}
