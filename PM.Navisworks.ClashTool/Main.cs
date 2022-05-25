793ad83b7946976d9772dd997b6921dd29c3a2a5
PMG-Jorge <jorge.alonso.x@pmgroup-global.com>
2022-05-25 13:52:30 +0200

    Add project files.

---
+++ PM.Navisworks.ClashTool/Main.cs
@@ -0,0 +1,20 @@
+﻿using Autodesk.Navisworks.Api.Plugins;
+using System.Windows;
+
+namespace PM.Navisworks.ClashTool
+{
+    [Plugin("PM.Navisworks.ClashTool",
+        "PMJF",
+        ToolTip = "Clash management tool.",
+        DisplayName = "Clash\nTool")]
+    public class Main : AddInPlugin
+    {
+        public override int Execute(params string[] parameters)
+        {
+            var activeDoc = Autodesk.Navisworks.Api.Application.ActiveDocument;
+            MessageBox.Show("Hello!");
+
+            return 0;
+        }
+    }
+}
